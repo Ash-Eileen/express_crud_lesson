3693c773361cf441073fddc0861c992bafb18f08
Ashley Smith <ash.mash.134@gmail.com>
2020-09-30 09:23:12 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const postRouter = require("./routes/posts_routes")
+
+const port = 3000
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use("/posts", postRouter)
+
+app.listen(port, () => {
+	console.log(`Blog express app listening on port ${port}`)
+})
